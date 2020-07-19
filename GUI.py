56b39a8d6a7bd65364db7b31848ff3dd45253a04
Apurv Shah <apurvshah@Apurvs-MBP.home>
2020-07-19 15:03:52 +0300

    Fixed GUI and PR.py according to new changes

--- GUI.py
+++ GUI.py
@@ -4,29 +4,55 @@ import tkinter as tk
 
 top = tk.Tk()
 Label(top, text = "Welcome to Project Reward: \n Enter the stock of your choice and chose the spread type").pack()
-canvas1 = tk.Canvas(top, width = 200, height = 120)
+canvas1 = tk.Canvas(top, width = 200, height = 160)
 # greetings = Label(text = "Hello")
 # greetings.pack()
 canvas1.pack()
+def setStock():
+	st = stock.get()
+	PR.setStock(st)
+
+def setDate():
+	d = date.get()
+	PR.setDate(d)
+
 stock = tk.Entry(top)
 canvas1.create_window(100,50,window=stock)
 
-def getStock():
-	st = stock.get()
-	PR.setstock(st)
+btn = Button(top, text = "Set Stock", command = setStock)
+canvas1.create_window(100,80,window=btn)
 
-btn = Button(top, text = "Set stock", command = getStock)
-canvas1.create_window(100,90,window=btn)
-PR.setstock(stock.get())
+date = tk.Entry(top)
+canvas1.create_window(100,110,window=date)
+
+btnDate = Button(top, text = "Set Date", command = setDate)
+canvas1.create_window(100,140,window=btnDate)
+
 def get_pds():
 	win = tk.Toplevel(top)
-	text = PR.Put_Debit_Spread()
+	text = PR.basicSpreads('putD')
 	Label(win , text = text).pack()
-	
+
+def get_cds():
+
+	win = tk.Toplevel(top)
+	text = PR.basicSpreads('callD')
+	Label(win , text = text).pack()
+
+def get_pcs():
+	win = tk.Toplevel(top)
+	text = PR.basicSpreads('putC')
+	Label(win , text = text).pack()
+
+def get_ccs():
+	win = tk.Toplevel(top)
+	text = PR.basicSpreads('callC')
+	Label(win , text = text).pack()
+
 btn1 = Button(top, text="Bearish Put Spread" , command = get_pds)
-btn2 = Button(top, text="Bearish Call Spread", command = PR.Call_Credit_Spread)
-btn3 = Button(top, text="Bullish Put Spread", command = PR.Put_Credit_Spread)
-btn4 = Button(top, text="Bullish Call Spread", command = PR.Call_Debit_Spread)
+btn2 = Button(top, text="Bearish Call Spread", command = get_ccs)
+btn3 = Button(top, text="Bullish Put Spread", command = get_pcs)
+btn4 = Button(top, text="Bullish Call Spread", command = get_cds)
 btn1.pack()
 btn2.pack()
 btn3.pack()
--- PR.py
+++ PR.py
@@ -6,57 +6,63 @@ import yfinance as yf
 import pytz
 import datetime
 from os import system, name
-date ='2020-08-21'
-stock =''
-def setstock(st):
+
+#global Variables
+
+stock, date = '', ''
+
+# getStock(st: String, d: String in date format) => calls: dataframe, puts: dataframe
+def setStock(st):
+    # global variables
     global stock
     stock = st
-setstock('UCO')
-def getDataPut():
-    date = '2020-08-21'
+def setDate(d):
+    global date
+    date = d
+
+def getDatafirst():
+
+    # assigning stock value and getting options data
     s = yf.Ticker(stock)
+    opt = s.option_chain(date)
+    call, put = opt.calls, opt.puts
+    
+    # Making our own Fair price column 'It is not exactly the market price, but very close.'
+    call['Fair Price'] = [i if i >= 0.01 else 0.01 for i in (call['ask']+call['bid'])/2]
+    put['Fair Price']  = [i if i >= 0.01 else 0.01 for i in (put['ask'] + put['bid'])/2]
+    
+    # Shortlisting data to a specific possible (rational) price range
     currentPrice = s.history(period = "max")['Close'].iloc[-1]
     oldPrice = s.history(period = "max")['Low'].iloc[-20]
-    priceRange = currentPrice - oldPrice
-    opt = s.option_chain(date)
-    puts =  opt.puts
-    puts['Fair Price']  = [i if i >= 0.01 else 0.01 for i in (puts['ask'] + puts['bid'])/2]
-    p = puts[puts['volume']>=int(puts['volume'].mean())]
-    l = p[['strike', 'Fair Price']]
-    j = p[['strike', 'lastPrice']]
-    if p['ask'].mean() != 0:
+    r = currentPrice - oldPrice
+    priceRange = [int((currentPrice - r).round()), int((currentPrice + r).round())]
+    
+    # Assigning values to calls and puts
+    calls = call[call['strike'] <= priceRange[1]]
+    puts = put[put['strike'] >= priceRange[0]]
+    
+    return calls, puts
+
+# getData(prediction: dataframe (can be calls or puts)) => n: dataframe
+def getData(prediction):
+    
+    # Shortlisting even more accoriding to good volume.
+    c = prediction[prediction['volume']>=int(prediction['volume'].mean())]
+    
+    if c['ask'].mean() != 0: # need to use time library later on
+        l = c[['strike', 'Fair Price']]
         m = l.set_index('strike').to_dict()
         n = m['Fair Price'] 
         print("Using Current Market Price")
+        
     else:
+        j = c[['strike', 'lastPrice']]
         m = j.set_index('strike').to_dict()
         n = m['lastPrice']
         print("Using Last Price")
+        
     return n
 
-def getDataCall():
-    date = '2020-08-21'
-    s = yf.Ticker(stock)
-    currentPrice = s.history(period = "max")['Close'].iloc[-1]
-    oldPrice = s.history(period = "max")['Low'].iloc[-20]
-    priceRange = currentPrice - oldPrice
-    opt = s.option_chain(date)
-    calls, puts = opt.calls, opt.puts
-    calls['Fair Price'] = [i if i >= 0.01 else 0.01 for i in (calls['ask']+calls['bid'])/2]
-    c = calls[calls['volume']>=int(calls['volume'].mean())]
-#     if t == 'Credit':
-#         c = calls[calls['inTheMoney'] == False]
-    l = c[['strike', 'Fair Price']]
-    j = c[['strike', 'lastPrice']]
-    if c['ask'].mean() != 0: #need to use time library for 
-        m = l.set_index('strike').to_dict()
-        n = m['Fair Price'] 
-        print("Using Current Market Price")
-    else:
-        m = j.set_index('strike').to_dict()
-        n = m['lastPrice']
-        print("Using Last Price")
-    return n
 
 def put_payoff(sT, k ,p):
     return np.where(sT<k, k-sT , 0)-p
@@ -124,194 +130,127 @@ def plotGraph(best_ratio, tittle, t):
     
 
 def appendBest(best_ratio, l, s, lp, sp, r, mrx, mrw):
-    best_ratio['Long(Buy)']=l
-    best_ratio['Short(Sell)']=s
-    best_ratio['Risk/Reward Ratio']=r
-    best_ratio['Short Premium'] =sp
+    
+    best_ratio['Long(Buy)'] = l
+    best_ratio['Short(Sell)'] = s
+    best_ratio['Risk/Reward Ratio'] = r
+    best_ratio['Short Premium'] = sp
     best_ratio['Long Premium'] = lp
     best_ratio['maxRisk'] = mrx
     best_ratio['maxReward'] = mrw
 
     
 def rnR(type, sL, sS, pL, pS):
+    
+    # credit spreads
     if type == 'callC' or type == 'putC':
         MaxReward = pS - pL
         MaxRisk = (sL - sS - MaxReward)
+        
         if type == 'putC':
             MaxRisk *= -1
-    if type == 'callD' or type == 'putD':
-#         print(sL,  sS)
+    
+    # debit spreads
+    elif type == 'callD' or type == 'putD':    
         MaxRisk = pL - pS
-#         print(MaxRisk)
         MaxReward = (sL - sS) - MaxRisk
+        
         if type == 'callD':
             MaxReward *= -1
+            
     return MaxRisk, MaxReward
 
 #Spread 1
-def Call_Credit_Spread():
-    #calls = getDataCall()
-    n = getDataCall()
-#     We need to check the risk, reward, break even point
-#     Then we need to check the ratio of the risk/reward
-#     Then we need to store the 'Transaction' of the least ratio
-# best_ratio: Dictionary to maintain the best available trade at a given time
-    best_ratio = {'Short(Sell)':0, 'Long(Buy)':0, 'Risk/Reward Ratio':((2**31)-1), 'Short Premium':0, 'Long Premium':0, 'maxRisk':0, 'maxReward':0}
-    # First loop to check what to short (sell)   
-    for short in n:
-        # Second loop to check what we're buying (long)
-        for long in n:
-            # In a credit call spread the long Strike cannot be lower or equal to the short strike price
-            if long <= short:
-                continue
-            else:
-                # Declaring variables to work with
-                strikeShort = short
-                strikeLong = long
-                
-                premiumShort = n[short]
-                premiumLong = n[long]
-                
-                # Figuring out the max risk vs reward in each spread to minimise the risk/reward ratio.
-                z = rnR('callC', strikeLong, strikeShort, premiumLong, premiumShort)
-                MaxRisk = z[0]
-                MaxReward = z[1]
-                
-                if MaxReward>0:
-                    Ratio =  MaxRisk/MaxReward
-                    if Ratio < best_ratio['Risk/Reward Ratio']:
-                        appendBest(best_ratio, long, short, premiumLong, premiumShort, Ratio, MaxRisk, MaxReward)
-                        
-    plotGraph(best_ratio, 'Call Credit Spread', 'callC')
-    return print("Best Call Credit Spread: ", best_ratio)
-#Call_Credit_Spread ()
-#Spread 2
-def Put_Credit_Spread():
-    n = getDataPut()
-#     We need to check the risk, reward, break even point
-#     Then we need to check the ratio of the risk/reward
-#     Then we need to store the 'Transaction' of the least ratio
-#     best_ratio: Dictionary to maintain the best available trade at a given time
-    best_ratio = {'Short(Sell)':0, 'Long(Buy)':0, 'Risk/Reward Ratio':((2**31)-1), 'Short Premium':0, 'Long Premium':0, 'maxRisk':0, 'maxReward':0}
+#Spread 1
+def basicSpreads(t):
+    calls = getDatafirst()[0]
+    puts = getDatafirst()[1]   
+
+    if t == 'callD' or t == 'callC':
+        n = getData(calls)
+    elif t == 'putC' or t == 'putD':
+        n = getData(puts)
     
-    # First loop to check what to long (buy) 
-    for long in n:
-        # Second loop to check what we're selling(short)
-        for short in n:
-            # In a put credit spread the strike price of what we're buying cannot be higher than strike price of what we're selling
-            if long >= short:
-                continue
-            
-            else:
-                #Decalring various variables
-                strikeShort = short
-                strikeLong = long
-                premiumShort = n[short]
-                premiumLong = n[long]
-                
-                #figuring out the max risk vs reward in each spread to minimise the risk/reward ratio.
-                z = rnR('putC', strikeLong, strikeShort, premiumLong, premiumShort)
-                MaxRisk = z[0]
-                MaxReward = z[1]
-                
-                if MaxReward>0:
-                    Ratio =  MaxRisk/MaxReward
-                    if Ratio < best_ratio['Risk/Reward Ratio']:
-                        appendBest(best_ratio, long, short, premiumLong, premiumShort, Ratio, MaxRisk, MaxReward)
-
-    plotGraph(best_ratio, 'Put Credit Spread', 'putC' )                                      
-    return print("Best Put Credit Spread: ", best_ratio)
-
-#Spread 3
-def Call_Debit_Spread():
-    n = getDataCall()
-#     We need to check the risk, reward, break even point
-#     Then we need to check the ratio of the risk/reward
-#     Then we need to store the 'Transaction' of the least ratio
-    best_ratio = {'Short(Sell)':0, 'Long(Buy)':0, 'Risk/Reward Ratio':((2**31)-1), 'Short Premium':0, 'Long Premium':0, 'maxRisk':0, 'maxReward':0}
-    # First loop to check what we're buying
-    for long in n:
-        # Second loop for what we're selling
-        for short in n:
-            # We don't want buying strike to be higher than selling strike (Call debit, sell higher strike and buy lower strike)
-            if long >= short:
-                continue
-            else:
-                
-                #Declaring variables
-                strikeShort = short
-                strikeLong = long
-                premiumShort = n[short]
-                premiumLong = n[long]
-                
-                z = rnR('callD', strikeLong, strikeShort, premiumLong, premiumShort)
-                MaxRisk = z[0]
-                MaxReward = z[1]
-                            
-                if MaxReward > 0:
-                    Ratio =  MaxRisk/MaxReward
-                    if Ratio < best_ratio['Risk/Reward Ratio']:
-                        appendBest(best_ratio, long, short, premiumLong, premiumShort, Ratio, MaxRisk, MaxReward)
-
-
-                        
-    plotGraph(best_ratio, 'Call Debit Spread', 'callC')                    
-    return print("Best Call Debit Spread: ", best_ratio)
-
-
-#Spread 4
-def Put_Debit_Spread():
-    n = getDataPut()
 #     We need to check the risk, reward, break even point
 #     Then we need to check the ratio of the risk/reward
 #     Then we need to store the 'Transaction' of the least ratio
 
     # best_ratio: Dictionary to maintain the best available trade at a given time
     best_ratio = {'Short(Sell)':0, 'Long(Buy)':0, 'Risk/Reward Ratio':((2**31)-1), 'Short Premium':0, 'Long Premium':0, 'maxRisk':0, 'maxReward':0}
-    
-    # First loop to check what we're buying
-    for long in n:
+   
+    # First loop to check what to short (sell)   
+    for short in n:
         
-        # Second loop for what we're selling
-        for short in n:
+        # Second loop to check what we're buying (long)
+        for long in n:
             
-            # We don't want buying strike to be smaller than selling strike (Put debit, buy higher strike and sell lower strike)
-            if long <= short:
-                continue
-            else:
-                
-                #Declaring variables
-                strikeShort = short
-                strikeLong = long
-                premiumShort = n[short]
-                premiumLong = n[long]
-                
-                # figuring out the max risk vs reward in each spread to minimise the risk/reward ratio.
-                z = rnR('putD', strikeLong, strikeShort, premiumLong, premiumShort)
-                MaxRisk = z[0]
-                MaxReward = z[1]
-        
-                if MaxReward>0:
-                    Ratio =  MaxRisk/MaxReward
-                    if Ratio < best_ratio['Risk/Reward Ratio']:
-                        appendBest(best_ratio, long, short, premiumLong, premiumShort, Ratio, MaxRisk, MaxReward)
+            # In a credit call spread the long Strike cannot be lower or equal to the short strike price
+            if t == 'putD' or t == 'callC':
+                if long > short:
 
-                        
-    plotGraph(best_ratio, 'Put Debit Spread', 'putD')  
-    return print(("Best Put Debit Spread: ", best_ratio))
+                    # Declaring variables to work with
+                    strikeShort = short
+                    strikeLong = long
 
-#Final Testing call for all spreads
-def finalSpreads():
-    print("\t\t~~~~~~~~~~~~~~~~~~~~~~~ Bullish for Date",date , "~~~~~~~~~~~~~~~~~~~~~~~~~ \n")
-    Call_Debit_Spread()
-    print("\n")
-    Put_Credit_Spread()
-    print("\n")
-    print("\t\t~~~~~~~~~~~~~~~~~~~~~~~ Bearish for Date",date ,"~~~~~~~~~~~~~~~~~~~~~~~~~~ \n")
-    Put_Debit_Spread()
-    print("\n")
-    Call_Credit_Spread()
-finalSpreads()
+                    premiumShort = n[short]
+                    premiumLong = n[long]
+                    
+                    z = rnR(t, strikeLong, strikeShort, premiumLong, premiumShort)
+                
+                    MaxRisk = z[0]
+                    MaxReward = z[1]
+
+                    if MaxReward > 0:
+                        Ratio =  MaxRisk/MaxReward
+                        if Ratio < best_ratio['Risk/Reward Ratio']:
+                            appendBest(best_ratio, long, short, premiumLong, premiumShort, Ratio, MaxRisk, MaxReward)
+
+               
+            elif t == 'putC' or t == 'callD': 
+                if long < short:
+
+                    # Declaring variables to work with
+                    strikeShort = short
+                    strikeLong = long
+
+                    premiumShort = n[short]
+                    premiumLong = n[long]
+
+                    # Figuring out the max risk vs reward in each spread to minimise the risk/reward ratio.
+                    z = rnR(t, strikeLong, strikeShort, premiumLong, premiumShort)
+                
+                    MaxRisk = z[0]
+                    MaxReward = z[1]
+
+                    if MaxReward > 0:
+                        Ratio =  MaxRisk/MaxReward
+                        if Ratio < best_ratio['Risk/Reward Ratio']:
+                            appendBest(best_ratio, long, short, premiumLong, premiumShort, Ratio, MaxRisk, MaxReward)
+
+#     plotGraph(best_ratio, 'Call Credit Spread', 'callC')
+    plotGraph (best_ratio, 'Spread', t)
+    return ("Best", t, "Spread: ", best_ratio)
+#setDate('2020-08-21')
+# setStock("UCO")
+# basicSpreads('callD')
+# print("\n")
+# basicSpreads('putC')
+# print("\n")
+# basicSpreads('callC')
+# print("\n")
+# basicSpreads('putD')
+# #Final Testing call for all spreads
+# def finalSpreads():
+#     print("\t\t~~~~~~~~~~~~~~~~~~~~~~~ Bullish for Date",date , "~~~~~~~~~~~~~~~~~~~~~~~~~ \n")
+#     Call_Debit_Spread()
+#     print("\n")
+#     Put_Credit_Spread()
+#     print("\n")
+#     print("\t\t~~~~~~~~~~~~~~~~~~~~~~~ Bearish for Date",date ,"~~~~~~~~~~~~~~~~~~~~~~~~~~ \n")
+#     Put_Debit_Spread()
+#     print("\n")
+#     Call_Credit_Spread()
+
 
 
 
