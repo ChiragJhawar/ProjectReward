0cec906a66f0b6a40d6c80d987d7f102cd16fef3
Apurv Shah <apurvshah@Apurvs-MBP.home>
2020-07-20 10:02:43 +0300

    Finsihed API routing and completed the class definition

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from PRaj import ProjectRewarder
+from PRApurv import ProjectRewarder
 
 worker = ProjectRewarder(None, None, None, None)
 
@@ -8,6 +8,10 @@ app = Flask(__name__)
 '''
     ./ngrok http 7500
     curl -X POST -d "ticker=GOOG" localhost:7500/api/set_ticker
+    curl -X POST -d "date=2020-08-21" localhost:7500/api/set_date
+    curl -X POST -d "flag=calls" localhost:7500/api/set_flag
+    curl -X POST -d "spread_type=credit" localhost:7500/api/set_type
+
 '''
 
 @app.route('/api/set_ticker', methods=['POST'])
@@ -16,5 +20,25 @@ def setTicker():
     response = worker.setTicker(request.form['ticker'])
     print(worker)
     return jsonify(response)
+
+@app.route('/api/set_date', methods=['POST'])
+def setDate():
+	response = worker.setDate(request.form['date'])
+	print(worker)
+	return jsonify(response)
+@app.route('/api/set_flag', methods=['POST'])
+def setFlag():
+	response = worker.setFlag(request.form['flag'])
+	print(worker)
+	return jsonify(response)
+@app.route('/api/set_type', methods=['POST'])
+def setType():
+	response = worker.setType(request.form['spread_type'])
+	print(worker)
+	return jsonify(response)
+@app.route('/api/spread/basic_spreads', methods=['GET'])
+def getSpread():
+	worker.getBasicSpread()
+	return jsonify(worker.best_ratio)
 
 app.run(debug=True, port=7500)
