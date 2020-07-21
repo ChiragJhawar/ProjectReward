0d3d467b069266a11808d9ce5ba8ca0ed6b94311
A.J. Uppal <ajuppal@AJs-MacBook-Pro.lan>
2020-07-21 16:38:17 -0700

    server update

--- Scripts/server.py
+++ Scripts/server.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from PRApurv import ProjectRewarder
+from flask_cors import CORS
 
 worker = ProjectRewarder(None, None, None, None)
 
 app = Flask(__name__)
+cors = CORS(app)
+
+print("CORRECT")
 
 '''
     ./ngrok http 7500
@@ -11,7 +15,7 @@ app = Flask(__name__)
     curl -X POST -d "date=2020-08-21" localhost:7500/api/set_date
     curl -X POST -d "flag=calls" localhost:7500/api/set_flag
     curl -X POST -d "spread_type=credit" localhost:7500/api/set_type
-    curl -X GET localhost:7500/api/spread/basic_spreads
+    curl -X POST -d "Stock=GOOG" localhost:7500/api/spread/basic_spreads
 
 '''
 
@@ -41,23 +45,21 @@ app = Flask(__name__)
 # def getSpread():
 # 	worker.getBasicSpread()
 # 	return jsonify(worker.best_ratio)
-
-worker.setTicker('GOOG')
-worker.setDate('2020-08-21')
-worker.setFlag('Calls')
-worker.setType('Credit')
-@app.route('/api/spread/basic_spreads', methods=['GET'])
+@app.route('/api/spread/basic_spreads', methods=['POST'])
 def getBasicSpread():
-	try:
-		# worker.setTicker(request.form['Stock'])
-		# worker.setDate(request.form['selectDate'])
-		# worker.setFlag(request.form['selectFlag'])
-		# worker.setType(request.form['selectType'])
-		worker.getBasicSpread()
-		return jsonify('Hello World')
-	except:
-		return jsonify({'status': 501})
-	
+    try:
+        data = request.json
+        print(data)
+        worker.setTicker(data['Stock'])
+        worker.setDate(data['selectDate'])
+        worker.setFlag(data['selectFlag'])
+        worker.setType(data['selectType'])
+        print(worker)
+        worker.getBasicSpread()
+        return jsonify(worker.best_ratio)
+    except Exception as e:
+        return jsonify({'status': 501, 'exception': e})
+
 
 
 app.run(debug=True, port=7500)
