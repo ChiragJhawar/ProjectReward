698d5fba6d4c5446b9742bd85a4a59d9679f0c61
Apurv <aashah@umass.edu>
2020-12-02 16:40:22 +0300

    Cleanup

---
+++ Scripts/build/precache-manifest.408f578dbe88e77dfddd5930f3f0c0eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "81400be5ec0b38af4c08",
+    "url": "/static/css/main.f7022531.chunk.css"
+  },
+  {
+    "revision": "81400be5ec0b38af4c08",
+    "url": "/static/js/main.61130f38.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "122e5e5463bcd51bf23a",
+    "url": "/static/css/2.957ba9c5.chunk.css"
+  },
+  {
+    "revision": "122e5e5463bcd51bf23a",
+    "url": "/static/js/2.6ea30382.chunk.js"
+  },
+  {
+    "revision": "977c7ae7229acb38bdef05fd8419e230",
+    "url": "/index.html"
+  }
+];
---
+++ Scripts/ui/src/footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="footer">
+    <p>This is some content in sticky footer</p>
+  </div>
+);
+
+export default Footer;
